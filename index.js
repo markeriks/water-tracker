0332ac4e9c1702d0565b37f48e2a709db1d23ae9
markeriks <markerik2016@gmail.com>
2025-02-20 20:14:49 +0200

    .

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let saveEl = document.getElementById("save-el")
-    let countEl = document.getElementById("count-el")
-    let count = 0
 
-    function increment() {
-        count += 1
-        countEl.textContent = count
-    }
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
-    function save() {
-        let countStr = count + " - "
-        saveEl.textContent += countStr
-        countEl.textContent = 0
-        count = 0
-    }
-});
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
